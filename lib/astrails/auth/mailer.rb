522a4cc3e4ffb5fd53049a79e83a455397e54a38
astrails <we@astrails.com>
2009-04-26 14:58:44 +0100

    added domain from global settings in mailer

--- lib/astrails/auth/mailer.rb
+++ lib/astrails/auth/mailer.rb
@@ -1,12 +1,11 @@
 module Astrails
   module Auth
     class Mailer < ActionMailer::Base
-      default_url_options[:host] = "authlogic_example.binarylogic.com"
+      domain = default_url_options[:host] = GlobalPreference.get(:domain) 
 
       def password_reset_instructions(user)
         subject       "Password Reset Instructions"
-        # FIXME: get this from settings file
-        from          "Password Reset <noreply@astrails.com>"
+        from          "Password Reset <noreply@#{domain}>"
         recipients    user.email
         sent_on       Time.now
         body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
