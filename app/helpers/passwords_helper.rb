19b7016a5e352c8e2e192a44c8da8cde39f07865
Vitaly Kushner <vitaly@astrails.com>
2009-09-30 00:09:05 +0200

    update password reset forms

--- app/helpers/passwords_helper.rb
+++ app/helpers/passwords_helper.rb
@@ -1,15 +1,5 @@
 module PasswordsHelper
-  def password_submit_label
-    if logged_in?
-      "Change"
-    elsif @user.active?
-      "Reset"
-    else
-      "Activate"
-    end
-  end
-
-  def password_form_title
+  def password_edit_title
     if logged_in?
       "Change Password"
     elsif @user.active?
@@ -19,11 +9,13 @@ module PasswordsHelper
     end
   end
 
-  def password_reset_submit_label
-    if @user.active?
-      "Send Password Reset instructions"
+  def password_edit_submit
+    if logged_in?
+      "Change"
+    elsif @user.active?
+      "Reset"
     else
-      "Send Activation instructions"
+      "Activate"
     end
   end
 end
