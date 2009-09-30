eb7bc8484d95044a067a3a95e8b62b63e86871ea
Vitaly Kushner <vitaly@astrails.com>
2009-09-30 03:38:01 +0200

    fix named routes

--- generators/astrails_user_auth/astrails_user_auth_generator.rb
+++ generators/astrails_user_auth/astrails_user_auth_generator.rb
@@ -17,8 +17,8 @@ class AstrailsUserAuthGenerator < Rails::Generator::Base
       m.route_name :login, '"/login", :controller => "user_session", :action => "new"'
 
       m.route_resources ':passwords'
-      m.route_name :edit_password, "'/password', :controller => 'passwords', :action => 'edit', :conditions => { :method => :get }"
-      m.route_name :update_password, "'/password', :controller => 'passwords', :action => 'update', :conditions => { :method => :put }"
+      m.route_name :password_edit, "'/password', :controller => 'passwords', :action => 'edit', :conditions => { :method => :get }"
+      m.route_name :password_update, "'/password', :controller => 'passwords', :action => 'update', :conditions => { :method => :put }"
       m.route_name :activate, "'/activate/:id', :controller => 'passwords', :action => 'edit'"
 
       # models
