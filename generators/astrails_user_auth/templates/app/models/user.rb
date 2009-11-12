57c29707647794f96a0c8efc458c2fdf4a987f9a
Vitaly Kushner <vitaly@astrails.com>
2009-11-12 05:19:03 +0200

    validations

--- generators/astrails_user_auth/templates/app/models/user.rb
+++ generators/astrails_user_auth/templates/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
+    c.merge_validates_format_of_email_field_options :live_validator =>
+      /^[A-Z0-9_\.%\+\-]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,4}|museum|travel)$/i
     c.validates_length_of_password_field_options =
       {:on => :update, :minimum => 4, :if => :has_no_credentials?}
     c.validates_length_of_password_confirmation_field_options =
@@ -8,4 +10,5 @@ class User < ActiveRecord::Base
   end
   include Astrails::Auth::Model
   attr_accessible :name, :password, :password_confirmation
+  validates_presence_of :name
 end
